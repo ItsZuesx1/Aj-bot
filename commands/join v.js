803ef02ece5cd1d5da590c41a65c6692249885cd
ItsZuesx1 <85587665+ItsZuesx1@users.noreply.github.com>
2021-08-26 12:13:11 +0300

    Create join v.js

---
+++ commands/join v.js
@@ -0,0 +1,13 @@
+module.exports ={
+  name : 'join v',
+  execute(message , args,channel){
+    if(!channel) return; message.channel.reply('please join any channel!');
+    channel.join().catch(err =>{
+      message.channel.reply('error 404 bitch'))};
+                         
+                         
+                         
+                         
+                         
+                        
+  
