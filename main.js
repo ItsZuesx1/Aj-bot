344eba50f00e029ecadb0ffdd046829232439788
ItsZuesx1 <85587665+ItsZuesx1@users.noreply.github.com>
2021-08-26 12:23:19 +0300

    Update main.js

--- main.js
+++ main.js
@@ -86,8 +86,8 @@ client.on('connect',connect => {
 if(!message.content.startswith(prefix) || message.author.bot return;
    const command = args.shift().toLowerCase();
      const channel = message.member.voice.channel;
-       if(!channel){ return; message.channel.reply('please join any channel!')};
+      /// if(!channel){ return; message.channel.reply('please join any channel!')};
      const args = message.content.slice(prefix.length).split(/ +/);
      if(command === 'join v'){
-          client.commands.get('join v').execute(message , args , channel)};
+          client.commands.get('join v').execute(message, args , channel)};
      client.login(process.env.BOT_TOKEN);
