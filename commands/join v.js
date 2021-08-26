bc448937485d3ee5f36cb58895bec02ab339d34b
ItsZuesx1 <85587665+ItsZuesx1@users.noreply.github.com>
2021-08-26 12:21:17 +0300

    Update join v.js

--- commands/join v.js
+++ commands/join v.js
@@ -1,7 +1,7 @@
 module.exports ={
   name : 'join v',
   execute(message , args,channel){
-    if(!channel){ return; message.channel.reply('please join any channel!')};
+  
     channel.join().catch(err =>{
       message.channel.reply('error 404 bitch'))};
                          
